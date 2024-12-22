97969a60a151bc53b0f664b81164cbcb551188a8
Kyrylo Ulianov <kirillulianow@gmail.com>
2024-12-22 21:20:42 +0200

    update

--- install.sh
+++ install.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env sh
 
+cd ~
+
 if [ -d "$HOME/.dotfiles" ]; then
     cd "$HOME/.dotfiles"
     git pull
 else
-    mkdir -p "$HOME/.dotfiles"
     echo "Select type of connection:"
     select connection in ssh http; do
         case $connection in
             ssh)
-                git clone git@github.com:ulyagram77/.dotfiles-linux.git
+                git clone git@github.com:ulyagram77/.dotfiles-linux.git .dotfiles
                 break
             ;;
             http)
-                git clone https://github.com/ulyagram77/.dotfiles-linux.git
+                git clone https://github.com/ulyagram77/.dotfiles-linux.git .dotfiles
                 break
             ;;
         esac
