94accb62dd5c87266a832d61925ebdb2176b7a86
Kyrylo Ulianov <74236970+ulyagram77@users.noreply.github.com>
2025-08-19 23:45:41 +0300

    Update wezterm.lua

--- dotfiles/.config/wezterm/wezterm.lua
+++ dotfiles/.config/wezterm/wezterm.lua
@@ -2,17 +2,29 @@ local wezterm = require 'wezterm'
 local config = {}
 
 config.color_scheme = 'Breeze (Gogh)'
-config.window_background_opacity = 0.90
+-- config.color_scheme = 'Andromeda'
+-- config.color_scheme = 'Apple System Colors'
+-- config.color_scheme = 'Ashes (base16)'
+-- config.color_scheme = 'Ayu Mirage (Gogh)'
+-- config.color_scheme = 'Atom (Gogh)'
+-- config.color_scheme = 'Atom'
+-- config.color_scheme = 'Batman'
+-- config.color_scheme = 'Bamboo'
+-- config.color_scheme = 'BlulocoDark'
+-- config.color_scheme = 'Breeze'
+-- config.color_scheme = 'Broadcast (Gogh)'
+config.window_background_opacity = 0.85
 config.window_padding = {
     left = 10,
     right = 10,
-    top = 10,
+    top = 0,
     bottom = 10,
 }
 config.enable_tab_bar = false
 config.enable_wayland = false
 config.font = wezterm.font 'FiraCode Nerd Font Mono'
-config.font_size = 13.0
+-- config.font = wezterm.font 'CaskaydiaCove Nerd Font Mono'
+config.font_size = 10.0
 config.bold_brightens_ansi_colors = false
 
 config.default_prog = { 'zsh' }
